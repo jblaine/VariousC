80dbe524ba305985c1805198682ad2998820b93d
jblaine <jblaine@kickflop.net>
2011-04-27 17:02:53 -0400

    .

---
+++ getnumprocs.c
@@ -0,0 +1,32 @@
+/*
+** Get and display the number of processors
+**
+*/
+
+#include <config.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    short int     numprocs = -1;
+
+#ifdef _SC_NPROCESSORS_ONLN
+    numprocs = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+#ifdef _SC_NPROC_ONLN
+    numprocs = sysconf(_SC_NPROC_ONLN);
+#endif
+
+    /* If the above failed, numprocs will be -1.  Just print it out */
+    /* as such which should be enough of an indicator that it is    */
+    /* unknown on this machine */
+    printf("%d\n", numprocs);
+
+    if (numprocs < 0) {
+        exit(1);
+    } else {
+        exit(0);
+    }
+}
---
+++ getphysmemkb.c
@@ -0,0 +1,38 @@
+/*
+** Get and display the (rough) physical memory size on a POSIXy box.
+**
+** Yippee:
+** http://en.wikipedia.org/wiki/Kilobyte
+** http://en.wikipedia.org/wiki/Megabyte
+**
+*/
+
+#include <config.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    unsigned long long int      physmem = -1;
+    unsigned long int           pages = -1;
+    unsigned long int           pagesizeKB = -1;
+
+#ifdef _SC_PHYS_PAGES
+    pages = sysconf(_SC_PHYS_PAGES);
+#endif
+
+#ifdef _SC_PAGESIZE
+    pagesizeKB = sysconf(_SC_PAGESIZE) / 1024;
+#endif
+
+    if ((pages != -1) && (pagesizeKB != -1)) {
+        physmem = pages * pagesizeKB;
+        printf("%lld\n", physmem);
+        exit(0);
+    } else {
+        printf("-1\n");
+        exit(1);
+    }
+}
+
